8a7d2ef696970e4ea2fda7b95c5ca02a998b0c6c
randyver <randyverdian99@gmail.com>
2023-11-22 20:19:40 +0700

    feat: completing start game function

--- src/initiating.pl
+++ src/initiating.pl
@@ -1,36 +1,124 @@
-:- dynamic(player/2).
-:- dynamic(turn/1).
-:- dynamic(troops/2).
-:- dynamic(owned/2).
-:- dynamic(placed/2).
+:- dynamic(player/2, turn/1, dice/2, maxDice/1, tie/1, winner/1).
 
-startGame :-
+% validasi jumlah pemain
+readPlayers(N) :-
     repeat,
     write('Masukkan jumlah pemain: '),
     read(N),
-    (N >= 2, N =< 4, !, initGame(N), playGame;
+    (between(2, 4, N), !;
      write('Mohon masukkan angka antara 2 - 4.\n'),
      fail).
 
-initGame(N) :-
-    retractall(player(_, _)),
-    retractall(turn(_)),
-    retractall(troops(_, _)),
-    retractall(owned(_, _)),
-    retractall(placed(_, _)),
-    retractall(initTroops(_)),
-    asserta(turn(1)),
-    initPlayers(N),
-    rollDiceForPlayers.
-
-initPlayers(N) :-
-    initPlayers(1, N).
-
+% inisialisasi pemain
+initPlayers(Current, N) :-
+    Current > N, !.
 initPlayers(Current, N) :-
     Current =< N,
-    write('Masukkan nama pemain '), write(Current), write(': '),
-    read(Name),
-    asserta(player(Current, Name)),
-    asserta(troops(Current, 0)),
+    readPlayerName(Current),
     Next is Current + 1,
     initPlayers(Next, N).
+
+% baca nama pemain
+readPlayerName(Current) :-
+    format('Masukkan nama pemain ~w: ', [Current]),
+    read(Name),
+    asserta(player(Current, Name)).
+
+% kocok dadu
+rollDiceForPlayers :-
+    findall(Name, player(_, Name), Names),
+    reverse(Names, ReversedNames),
+    rollDiceForPlayers(ReversedNames).
+
+rollDiceForPlayers([]).
+rollDiceForPlayers([Player | Rest]) :-
+    random(1, 13, DiceRoll),
+    asserta(dice(Player, DiceRoll)),
+    format('~w melempar dadu dan mendapatkan ~w.\n', [Player, DiceRoll]),
+    rollDiceForPlayers(Rest),
+    updateMaxDice(DiceRoll),
+    updateTie(Player, DiceRoll).
+
+% nilai dadu maksimal
+updateMaxDice(DiceRoll) :-
+    maxDice(Max),
+    DiceRoll > Max,
+    retract(maxDice(Max)),
+    asserta(maxDice(DiceRoll)), !.
+updateMaxDice(DiceRoll) :-
+    maxDice(Max),
+    DiceRoll =< Max, !.
+updateMaxDice(DiceRoll) :-
+    asserta(maxDice(DiceRoll)).
+
+% cek dadu seri
+updateTie(Player, DiceRoll) :-
+    maxDice(Max),
+    DiceRoll = Max,
+    retractall(tie(_)),
+    asserta(tie(Player)), !.
+updateTie(_, _).
+
+% cek jika ada dadu terbesar yang sama
+checkTie :-
+    findall(Player, tie(Player), Ties),
+    length(Ties, N),
+    N > 1,
+    rollDiceForTie, !.
+checkTie :-
+    findall(Player, tie(Player), Ties),
+    length(Ties, N),
+    N = 1,
+    Ties = [Winner],
+    asserta(winner(Winner)).
+
+% kocok ulang dadu
+rollDiceForTie :-
+    retractall(dice(_, _)),
+    retractall(maxDice(_)),
+    retractall(tie(_)),
+    forall(tie(Player), (rollDiceForPlayer(Player), checkTie)).
+
+% urutan pemain
+sortPlayers :-
+    findall(Dice-Name, dice(Name, Dice), Pairs),
+    keysort(Pairs, SortedPairs),
+    reverse(SortedPairs, ReversedPairs),
+    findall(Winner, winner(Winner), Winners),
+    (Winners = [SingleWinner] -> Winner = SingleWinner; Winner = Winners),
+    write('\n'),
+    write('Urutan pemain berdasarkan nilai dadu: '),
+    printPlayerNames(ReversedPairs, Winner),
+    write('\n').
+
+% print urutan pemain dan urutan pertama
+printPlayerNames([], Winner) :-
+    write('.\n'),
+    write('Pemenang lemparan dadu adalah '), write(Winner), write('.\n').
+
+printPlayerNames([Dice-Name | Tail], Winner) :-
+    format('~w', [Name]),
+    (Tail == [] -> write(''); write(' - ')),
+    printPlayerNames(Tail, Winner).
+
+% print jumlah tentara
+printTroops :-
+    findall(Name, player(_, Name), Players),
+    length(Players, NumPlayers),
+    calculateTroops(NumPlayers, Troops),
+    format('Setiap pemain mendapatkan ~w Troops.', [Troops]).
+
+calculateTroops(2, 24) :- !.
+calculateTroops(3, 16) :- !.
+calculateTroops(4, 12) :- !.
+calculateTroops(_, 0).
+
+% fungsi start game
+startGame :-
+    readPlayers(N),
+    initPlayers(1, N),
+    write('\n'),
+    rollDiceForPlayers,
+    checkTie,
+    sortPlayers,
+    printTroops.
