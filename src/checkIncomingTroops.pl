6abcd13bf24a571ce18a429c30281c726942f57c
ganadipa <13522066@mahasiswa.itb.ac.id>
2023-11-28 14:50:24 +0700

    Fix minor bug check incoming  troops

--- src/checkIncomingTroops.pl
+++ src/checkIncomingTroops.pl
@@ -1,8 +1,12 @@
+main_CIT:-
+    add_additional_troops(p2, 123),
+    checkIncomingTroops(p2).
+
 /* checkIncomingTroops(p2).*/
 checkIncomingTroops(Player):-
     write('\nNama                                 :  '),
     player_name(Player, Name),
-    write(Name)
+    write(Name),
 
     write('\nTotal wilayah                        :  '),
     region_owned_length(Nama, TotalWilayah),
@@ -15,9 +19,9 @@ checkIncomingTroops(Player):-
     write('\nBonus                                :  '),
     write('\nBonus                                :  '),
 
-    
+
     write('\nTotal tentara tambahan               :  '),
     total_additional_troops(Player, TotalAdditionalTroops),
     write(TotalAdditionalTroops).
 
-:- initialization(checkIncomingTroops(p2)).
+:- initialization(main_CIT).
