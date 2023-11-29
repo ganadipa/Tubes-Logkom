f032898d29b0ff1987ab8de2c8796ae0e7eebbc5
ganadipa <13522066@mahasiswa.itb.ac.id>
2023-11-29 17:15:07 +0700

    PlaceTroops

--- src/placeTroops.pl
+++ src/placeTroops.pl
@@ -1,11 +1,20 @@
 placeTroops(RegionCode, TroopCount) :-
     current_player(Player),
     format('~w meletakkan ~w tentara di wilayah ~w.~n', [Player, TroopCount, RegionCode]),
+    total_additional_troops(Player, Remaining),
     total_troops(RegionCode, TotalTroops),
+
+
     NewTotalTroops is TotalTroops + TroopCount,
+    RemainingTroops is Remaining - TroopCount,
+
     retract(total_troops(RegionCode, _)),    
     assertz(total_troops(RegionCode, NewTotalTroops)),
-    RemainingTroops is 24 - NewTotalTroops,
+
+    retract(total_additional_troops(Player, _)),
+    assertz(total_additional_troops(Player, RemainingTroops)),
+
+
     format('Terdapat ~w tentara yang tersisa.~n', [RemainingTroops]),
     next_player,
     current_player(NextPlayer),
