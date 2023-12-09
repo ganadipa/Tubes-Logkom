87a05f2f0d3e5ecc52c8f0f932f8dffd2025bff8
ganadipa <13522066@mahasiswa.itb.ac.id>
2023-12-09 23:34:33 +0700

    Add validator utils

--- src/utils.pl
+++ src/utils.pl
@@ -1,7 +1,46 @@
-sum_list(List, Sum) :-
+sum_list_naive(List, Sum) :-
     sum_list_helper(List, 0, Sum).
 
 sum_list_helper([], Sum, Sum).
 sum_list_helper([H|T], Acc, Sum) :-
     NewAcc is Acc + H,
     sum_list_helper(T, NewAcc, Sum).
+
+int_validator(Min, Max, Value, String) :-
+    (   Value >= Min,
+        Value =< Max
+    ->  true  
+    ;   write(String), nl, fail
+    ).
+
+player_validator(Player, String) :-
+    total_player(Total),
+    (
+        Total == 2 -> 
+        (   
+            (Player == p3; Player == p4) -> 
+            (
+                write(String),
+                fail
+            )
+
+        ;   true
+        ); Total == 3 -> 
+        (
+            (Player == p4) -> 
+            (
+                write(String),
+                fail
+            )
+        ;   true
+        ); true
+    ).
+
+code_validator(Code, String):-
+    (
+        \+ region(Code) -> (
+            write(String),
+            fail
+        ); true
+    ).
+
