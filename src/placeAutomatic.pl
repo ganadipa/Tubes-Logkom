de121fa3506848c99d53487eac2430f81cbadaa5
ganadipa <13522066@mahasiswa.itb.ac.id>
2023-12-10 13:39:04 +0700

    Add endturn after initiating

--- src/placeAutomatic.pl
+++ src/placeAutomatic.pl
@@ -24,6 +24,42 @@ placeAutomatic:-
     Remainder is Remaining - (Number*Length),
     
     iterate_and_place(CodeList, Number, Remainder),
+    
+    (
+        total_additional_troops(p1, _)  -> 
+            (
+                total_additional_troops(p1, Count1),
+                Sum1 is Count1
+            ); Sum1 is 0
+    ),
+    (
+        total_additional_troops(p2, _)  -> 
+            (
+                total_additional_troops(p2, Count2),
+                Sum2 is Count2 + Sum1
+            ); Sum2 is Sum1
+    ),
+    (
+        total_additional_troops(p3, _)  -> 
+            (
+                total_additional_troops(p3, Count3),
+                Sum3 is Count3 + Sum2
+            ); Sum3 is Sum2
+    ),
+    (
+        total_additional_troops(p4, _)  -> 
+            (
+                total_additional_troops(p4, Count4),
+                Sum4 is Count4 + Sum3
+            ); Sum4 is Sum3
+    ),
+
+
+
+    (
+        
+        ((Sum4) == 0)  -> (endTurn); true
+    ),
     next_player.
 
 placeTroops_quietly(Region, TroopCount) :-
@@ -36,4 +72,6 @@ placeTroops_quietly(Region, TroopCount) :-
     format('\n~w berhasil meletakkan ~w di wilayah ~w, sehingga total ~w berada di wilayah ini.', [Name, TroopCount, Region, NewTotalTroops]),
 
     retract(total_troops(Region, _)),    
-    assertz(total_troops(Region, NewTotalTroops)).
+    assertz(total_troops(Region, NewTotalTroops))
+    .
+
--- src/placeTroops.pl
+++ src/placeTroops.pl
@@ -1,7 +1,8 @@
 placeTroops(Region, TroopCount) :-
     total_additional_troops(Player, Remaining),
-    int_validator(0, Remaining, TroopCount, 'Ngaco troopcountnya'),
-    
+    int_validator(0, Remaining, TroopCount, 'Invalid number.'),
+    code_validator(Region, 'Invalid code.'),
+
     current_player(Player),
     region_owner(Region, RegionOwner),
     !,
@@ -36,4 +37,40 @@ placeTroops(Region, TroopCount) :-
     format('Terdapat ~w tentara yang tersisa.\n', [RemainingTroops]),
     next_player,
     current_player(NextPlayer),
-    format('Giliran ~w untuk meletakkan tentaranya\n', [NextPlayer]).
+    format('Giliran ~w untuk meletakkan tentaranya\n', [NextPlayer]),
+    
+    (
+        total_additional_troops(p1, _)  -> 
+            (
+                total_additional_troops(p1, Count1),
+                Sum1 is Count1
+            ); Sum1 is 0
+    ),
+    (
+        total_additional_troops(p2, _)  -> 
+            (
+                total_additional_troops(p2, Count2),
+                Sum2 is Count2 + Sum1
+            ); Sum2 is Sum1
+    ),
+    (
+        total_additional_troops(p3, _)  -> 
+            (
+                total_additional_troops(p3, Count3),
+                Sum3 is Count3 + Sum2
+            ); Sum3 is Sum2
+    ),
+    (
+        total_additional_troops(p4, _)  -> 
+            (
+                total_additional_troops(p4, Count4),
+                Sum4 is Count4 + Sum3
+            ); Sum4 is Sum3
+    ),
+
+    (
+        
+        ((Sum4) == 0)  -> (endTurn); true
+    )
+    .
+
